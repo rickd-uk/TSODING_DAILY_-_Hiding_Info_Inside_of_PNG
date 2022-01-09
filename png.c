6415b6372257a3b778d31bd530fe38b1643806bd
Rick D <78909973+rickd-uk@users.noreply.github.com>
2022-01-09 20:08:07 +0900

    add: reverse_bytes function

--- png.c
+++ png.c
@@ -8,7 +8,7 @@
 #define PNG_SIG_CAP 8 
 const uint8_t png_sig[PNG_SIG_CAP] = {137,80,78,71,13,10,26,10 };
 
-void read_buffer_or_panic(FILE *file, uint8_t *buf,  size_t buf_cap) {
+void read_bytes_or_panic(FILE *file, void *buf,  size_t buf_cap) {
 
     size_t n = fread(buf, sizeof(buf_cap),  1, file);
 if (n != 1){
@@ -31,6 +31,23 @@ void print_bytes( uint8_t *buf,  size_t buf_cap){
  printf("\n");
 }
 
+void reverse_bytes(void *buf0,  size_t buf_cap) {
+  uint8_t *buf = buf0;
+  //printf("%zu\n", buf_cap/2);
+
+   for (size_t i  = 0; i < buf_cap/2; ++i) {
+     uint8_t t = buf[i];
+     printf("buf[%zu] %u\n", i, buf[0]);
+     buf[i] = buf[buf_cap - i - 1];
+     buf[buf_cap - i - 1] = t;
+   }
+}
+
+//  3 6 2 8 
+//  t =                                3                       6                         2
+// buf[i]                           4-0-1=buf[3]     4-1-1=buf[2]      4-2-1=buf[1]
+// buf[buf_cap - i - 1]      buf[3]=8           buf[2]=2             buf[1]=6
+
 int main(int argc, char **argv) {
 
   (void) argc;
@@ -52,7 +69,7 @@ if (input_file == NULL) {
 }
 
 uint8_t  sig[PNG_SIG_CAP];
-read_buffer_or_panic(input_file, sig, PNG_SIG_CAP);
+read_bytes_or_panic(input_file, sig, PNG_SIG_CAP);
 print_bytes(sig, PNG_SIG_CAP);
 
 if (memcmp(sig, png_sig, PNG_SIG_CAP) != 0) {
@@ -60,9 +77,11 @@ if (memcmp(sig, png_sig, PNG_SIG_CAP) != 0) {
  exit(1);
 }
 
- printf("%s is a valid PNG image\n", input_filepath); 
 
-
+uint32_t chunk_sz;
+read_bytes_or_panic(input_file, &chunk_sz, sizeof(chunk_sz)); 
+reverse_bytes(&chunk_sz, sizeof(chunk_sz));
+printf("Chunk size: %u\n", chunk_sz);
 
 fclose(input_file);
 
